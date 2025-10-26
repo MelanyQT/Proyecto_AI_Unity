368c2588a4d79fcf1d1da1c381f27afc7d8d9535
melany <mq32129@gmail.com>
2025-10-26 10:46:39 -0500

    Agregar scripts C# de los modelos de IA

---
+++ NPCMultiPatrol (1).cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCMultiPatrol : MonoBehaviour
+{
+    public Transform[] patrolPoints;
+    private NavMeshAgent agent;
+    private int currentPointIndex = 0;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        if (patrolPoints.Length > 0)
+        {
+            agent.SetDestination(patrolPoints[currentPointIndex].position);
+        }
+    }
+
+    void Update()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            currentPointIndex = (currentPointIndex + 1) % patrolPoints.Length;
+            agent.SetDestination(patrolPoints[currentPointIndex].position);
+        }
+    }
+}
